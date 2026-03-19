f4cb67cb3cce6b515caf4ee17235ad8e6fbd61f4
Madhav Bhatta <madhavbhatta11@gmail.com>
2026-03-19 20:27:20 +0545

    Add loops examples in day2task3.py

---
+++ day2task3.py
@@ -0,0 +1,31 @@
+#loops
+#for loop
+for i in range(0,41):
+    print(i)
+#about loops
+#for loop
+fruit="Apple, ball"
+for i in fruit:
+  print(i)    
+flist=["apple","banana","cfruit"]
+for j in flist:
+  print(j)
+  for k in j:
+    print(k)    
+    # print(j)
+# Range 
+for i in range(0,10):# print up to 9
+  print(i)
+for m in range(0,11,2):
+  print(m)
+
+for n in range(1,10,2):
+  print(n)
+#do-while loop in python
+while true:
+  num=int(input("Enter the number"))
+  if num>0:
+    print("it is positive number")
+    break
+  else:
+    print 
