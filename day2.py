26c5647ea763592ae2d530a2fcc28c59c8ee6337
Madhav Bhatta <madhavbhatta11@gmail.com>
2026-03-19 16:52:02 +0545

    Implement match-case statement for number input

--- day2.py
+++ day2.py
@@ -1,1 +1,12 @@
 #match _ case statement
+num = int(input("Enter the number: "))
+
+match num:
+    case 0:
+        print('The number is "Zero"')
+    case 1:
+        print("The number is 1")
+    case 2:
+        print("The number is <= 2")
+    case _:
+        print("The value is:", num)
