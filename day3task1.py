c01563b46b8bdb5345c44443d9deda1039b99adc
Madhav Bhatta <madhavbhatta11@gmail.com>
2026-03-21 20:16:49 +0545

    Add list operations example in day3task1.py
    Implement basic list operations in Python including input, conditionals, appending, inserting, removing, popping, and clearing the list.

---
+++ day3task1.py
@@ -0,0 +1,19 @@
+#data types in python
+#1 lists in pyton
+list1=[input(" first word "),input("second word "),input("third word ")]
+if list1[0]=="hello":
+    print("first word is hello")
+else:
+    print("first word is not hello")
+print(list1)
+print(list1[0:2])
+list1.append(input("fourth word "))
+print(list1)
+list1.insert(2,input("fifth word "))    
+print(list1)
+list1.remove(input("enter word to remove "))    
+print(list1)
+list1.pop(0)
+print(list1)
+list1.clear()   
+print(list1))
